3c2c608fceadbbc30e0957dc8aa3e1eb338d1b74
Denis Doinikov <sharfelinna@gmail.com>
2026-04-16 19:14:50 +0300

    working logic without creating files, need rework with info.json and progress bar, mb work with e 409

--- main.py
+++ main.py
@@ -2,17 +2,37 @@ import requests
 from secret import yd_token
 import json
 import os
-
+import io
+from pprint import pprint
 
 def create_url(url, method):
     return f'{url}{method}'
+
+
+def handle_exceptions(e, response=None, YD=False):
+    if isinstance(e, requests.exceptions.Timeout):
+        return 'Таймаут'
+    elif isinstance(e, requests.exceptions.HTTPError):
+        if response is not None:
+            return f'Ошибка {response.status_code}'
+    elif isinstance(e, requests.exceptions.RequestException):
+        return f'Ошибка соединения: {e}'
+    return 'Неизвестная ошибка'
 
 
 class cats_API:
     url = 'https://cataas.com/'  
     
     def get_pic_w_text(self, text):
-        response = requests.get(create_url(self.url, f'cat/says/:{text}'))
+        response = None
+        
+        try:
+            response = requests.get(create_url(self.url, f'cat/says/{text}'), timeout=10)
+            response.raise_for_status()
+        
+        except requests.exceptions.RequestException as e:
+            return handle_exceptions(e, response)
+        
         return response.content
 
 
@@ -22,32 +42,86 @@ class yd_API:
     def __init__(self, token):
         self.token = token
         self.headers = {'Authorization': f'OAuth {self.token}'}
+        self._file_links = []
     
     def create_folder(self,path):
         params = {'path': f'{path}'}
-        response = requests.put(create_url(self.url, f'resources'),
-                                headers=self.headers,
-                                params=params,
-                                timeout=10
-                                )
+        response = None
+        
+        try:
+            response = requests.put(create_url(self.url, f'resources'),
+                                    headers=self.headers,
+                                    params=params,
+                                    timeout=10,
+                                    )
+            response.raise_for_status()
+        
+        except requests.exceptions.RequestException as e:
+            return handle_exceptions(e, response)
     
     def add_file(self, path, content):
         params = {'path': path}
-        response = requests.get(create_url(self.url, f'resources/upload'),
-                                headers=self.headers,
-                                params=params,
-                                timeout=10
-                                )
+        response = None
+        up_resp = None
+        
+        try:
+            response = requests.get(create_url(self.url, f'resources/upload'),
+                                    headers=self.headers,
+                                    params=params,
+                                    timeout=10
+                                    )
+            response.raise_for_status()
+        
+        except requests.exceptions.RequestException as e:
+            return handle_exceptions(e, response)
+        except KeyError:
+            return 'Ошибка: нет ссылки для загрузки'
         upl_link = response.json()['href']
         
-        with open(content, 'rb') as f:
-            up_resp = requests.put(upl_link, files={'file': f}, timeout=30)
-            # print(f"Upload status: {up_resp.status_code}")
-        return {
-            'file_name': os.path.basename(path),
-            'path': path,
-            'size': os.path.getsize(content)
-        }
+        try:
+            up_resp = requests.put(upl_link, files={'file': content}, timeout=30)
+        
+        except requests.exceptions.RequestException as e:
+            return handle_exceptions(e, up_resp)
+        
+        self._file_links.append(f'{create_url(self.url, path)}')
+        
+        return
+    
+    def create_info_json(self):
+        resp = None
+        file_info = {}
+        
+        for path in self._file_links:
+            params = {'path': path}
+            
+            try:
+                resp = requests.get(
+                    path,
+                    headers=self.headers,
+                    params=params,
+                    timeout=10
+                )
+                resp.raise_for_status()
+            
+            except requests.exceptions.RequestException as e:
+                print(f'{handle_exceptions(e, resp)}')
+                continue
+            
+            data = resp.json()
+            file_info['name'] = {
+                'path': data['path'],
+                'size': data['size'],
+                'url': path
+            }
+        
+        with open('file_info.json', 'w') as f:
+            json.dump(file_info, f, ensure_ascii=False, indent=2)
+        
+        return
+    
+    def get_last_upl(self):
+        
 
 
 class dogs_API:
@@ -59,6 +133,8 @@ class dogs_API:
         self._list_files = []
     
     def get_breed(self):
+        resp = None
+        
         try:
             resp = requests.get(create_url(self.url, 'images'), timeout=10)
             resp.raise_for_status()
@@ -68,21 +144,17 @@ class dogs_API:
             if message:
                 return True, 'Порода существует'
             return False, 'Нет данный о породе'
-            
-        except requests.exceptions.Timeout:
-            return False, 'Таймаут'
-    
-        except requests.exceptions.HTTPError:
-            return False, f'Ошибка: {resp.status_code}'
-    
+        
         except requests.exceptions.RequestException as e:
-            return False, f'Ошибка соединения: {e}'
+            return False, handle_exceptions(e, resp)
     
     def get_subbreed(self):
         check_success, check_msg = self.get_breed()
         
         if not check_success:
             return check_msg
+        
+        resp = None
         
         try:
             resp = requests.get(create_url(self.url, 'list'), timeout=10)
@@ -106,8 +178,8 @@ class dogs_API:
                     img_resp.raise_for_status()
                     
                     self.sub_breed_data[s_breed] = {
-                        'img': img_resp.content,
-                        'file_name': file_name
+                        'img': io.BytesIO(img_resp.content),
+                        'file_name': f'{self.breed}_{s_breed}_{file_name}'
                     }
                 
                 except requests.exceptions.Timeout:
@@ -122,60 +194,43 @@ class dogs_API:
             
             return f'Скачано {len(self.sub_breed_data)} из {len(sub_breed_list)} под пород'
         
-        except requests.exceptions.Timeout:
-            return 'Таймаут'
-        except requests.exceptions.HTTPError:
-            return f'Ошибка: {resp.status_code}'
         except requests.exceptions.RequestException as e:
-            return f'Ошибка соединение: {e}'
-
-    def create_pics(self):
-        if self.sub_breed_data:
-            for e, s_breed in enumerate(self.sub_breed_data.values()):
-                f_name = f'{self.breed}_{list(self.sub_breed_data.keys())[e]}_{s_breed["file_name"]}'
-                with open(f_name, 'wb') as f:
-                    f.write(s_breed['img'])
-                self._list_files.append(f_name)
+            return handle_exceptions(e, resp)
 
 
 def dz():
     #variables
-    _text = input('Введите текст для котика:')
-    _group = input('Введите название группы:')
-    _breed = input('Введите название породы песеля')
+    _text = input('Введите текст для котика: ')
+    _group = input('Введите название группы: ')
+    _breed = input('Введите название породы песеля: ')
     
     # get cat pic
     cat_call = cats_API()
     pic = cat_call.get_pic_w_text(_text)
     
-    # save cat pic
-    with open(f'{_text}.png', 'wb') as f:
-        f.write(pic)
+    # save cat pic obj
+    pic_obj = io.BytesIO(pic)
     
     # get dogs_sub_breeds
     dog_call = dogs_API(_breed)
     dog_call.get_subbreed()
-    
-    #get dog pics
-    dog_call.create_pics()
     
     # create folders
     yd_call = yd_API(yd_token)
     yd_call.create_folder(_group)
     yd_call.create_folder(_breed)
     
-    # work with files
-    file_info = yd_call.add_file(f'{_group}/{_text}', f'{_text}.png')
+    # work with YD files
+    # cat pic upl
+    yd_call.add_file(f'{_group}/{_text}', pic_obj)
     
-    for file in dog_call._list_files:
-        # print(f'{file.removesuffix(".jpg")}')
-        yd_call.add_file(f'{_breed}/{file.removesuffix(".jpg")}', f'{file}')
+    # dogs pic upl
+    for s_breed in dog_call.sub_breed_data.values():
+        yd_call.add_file(f'{_breed}/{s_breed["file_name"]}', s_breed['img'])
+
+    # yd_call.create_info_json()
     
-    with open('file_info.json', 'w', encoding='utf-8') as f:
-        json.dump(file_info, f, indent=2, ensure_ascii=False)
-    
-    print(f'Файл загружен - {file_info}')
-    print(f'Информация сохранена в file_info.json')
+
 
 def main():
     dz()
